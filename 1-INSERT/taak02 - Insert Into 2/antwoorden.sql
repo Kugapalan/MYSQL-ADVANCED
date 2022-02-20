ee37fb9468a9bb3b38747bbfbcb093ffc5ef8e2c
Kugapalan <kugapalan.ganeshwaran@student.rocva.nl>
2022-02-20 20:30:10 +0100

    Opdracht Afgerond

--- 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
+++ 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
@@ -1,1 +1,12 @@
 
+1.
+INSERT INTO studenten (voornaam,achternaam,geboortedatum,woonplaats)
+VALUES ('Ronald','Weasley','2000-03-01','Amsterdam')
+
+2.
+INSERT INTO studenten (voornaam,achternaam,geboortedatum,woonplaats)
+VALUES ('Noah','Steen','2004-07-18','Roermond')
+
+3.
+INSERT INTO studenten (voornaam,achternaam,geboortedatum,woonplaats) 
+VALUES ('Maëlys','Mathis','2003-03-21','Breda');
