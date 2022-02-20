c34e51a3aaee63a6493229b5fb514ab044359f82
Kugapalan <kugapalan.ganeshwaran@student.rocva.nl>
2022-02-20 20:13:08 +0100

    Opdracht afgerond

--- 1-INSERT/taak01 - Insert Into/antwoorden.sql
+++ 1-INSERT/taak01 - Insert Into/antwoorden.sql
@@ -1,1 +1,23 @@
+1.
+INSERT INTO studenten (student_id,voornaam,achternaam,klas,adres,postcode,geboortedatum,woonplaats)
+VALUES (5,'Tom','Horst','9A','Hemsterhuisstraat 2','1067DC','2002-05-05','Amsterdam')
 
+2.
+INSERT INTO studenten (student_id,voornaam,achternaam,klas,adres,postcode,geboortedatum,woonplaats)
+VALUES (6,'Henk','Van Balen','9A','Albert Cuypstraat 1','1234AB','2000-05-18','Amsterdam')
+
+3.
+INSERT INTO studenten (student_id,voornaam,achternaam,klas,adres,postcode,geboortedatum,woonplaats) 
+VALUES (7,'Piet','Pieters','9A','Albert Cuypstraat 5','1234AB','2001-04-28','Ouderkerk');
+
+4.
+INSERT INTO studenten (student_id,voornaam,achternaam,klas,adres,postcode,geboortedatum,woonplaats) 
+VALUES (8,'Johanna','Eduardo','9C','Pieter Aertszstraat 44','1073CD','2001-09-02','Hoorn');
+
+5.
+INSERT INTO studenten (student_id,voornaam,achternaam,klas,adres,postcode,geboortedatum,woonplaats) 
+VALUES (9,'Angela','Mohammed','9C','Piet Mondriaanplein 14','1299CD','2002-05-17','Amstelveen');
+
+6.
+INSERT INTO studenten (student_id,voornaam,achternaam,klas,adres,postcode,geboortedatum,woonplaats) 
+VALUES (10,'Kyle','Green','9C','Amstelveenseweg 1', '1482FD','2003-05-08','Amsterdam');
